a0c3b0067ea727e12b7a38d975f93346ccae0ae2
0p3r4t0r <robert83sutherland@gmail.com>
2023-12-19 06:42:17 +0900

    better pipeline script, clean dir names

--- pipeline/awsIcons.sh
+++ pipeline/awsIcons.sh
@@ -1,21 +1,30 @@
 # Process files from
 # https://aws.amazon.com/architecture/icons/
 
-if [ ${PWD##*/}  = 'aws-gameplan' ]; then
+awsIconsPath='../assets/awsIcons'
+
+rm -rf $awsIconsPath
+cp -r $1 $awsIconsPath
+
+if [ ${PWD##*/}  = 'pipeline' ]; then
     # remove everyting but the svgs
-    find ./assets/awsIcons ! -name '*.svg' -type f -exec rm -f {} +
+    find $awsIconsPath ! -name '*.svg' -type f -exec rm -f {} +
 
     # remove extra sizes
     for size in 16 32 48; do
-        find ./assets/awsIcons -name "*$size" -type d -exec rm -rf {} +
+        find $awsIconsPath -name "*$size" -type d -exec rm -rf {} +
     done
 
-    # move files from the *64 directories to their parent directory
-    # TODO: make this idempotent
-    find ./assets/awsIcons -name "*_64.svg" -type f -execdir mv {} ../ \;
+    # move files from the *64 directories to their parents
+    find $awsIconsPath -name "*_64.svg" -type f -execdir mv {} ../ \;
 
-    # remove the empty directories
-    find ./assets/awsIcons -name "*64" -type d -exec rmdir {} +
+    # remove empty directories
+    find $awsIconsPath -name "*64" -type d -exec rmdir {} +
+
+    # remove date from the ends of directories
+    for i in $awsIconsPath/*; do
+        mv $i ${i%_*}
+    done
 else
     echo "You are not in the right directory"
 fi
