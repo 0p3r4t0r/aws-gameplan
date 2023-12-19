972bbb4bbae4c00f1621357c7481a866e2a6bf0c
0p3r4t0r <robert83sutherland@gmail.com>
2023-12-19 10:01:06 +0900

    migrate build and watch to scripts

---
+++ build.mjs
@@ -0,0 +1,28 @@
+import * as esbuild from 'esbuild';
+import * as fs from 'fs';
+
+const PATH = 'public-dev';
+
+console.log('Building dev...');
+
+// Clean
+fs.rmSync(PATH, { recursive: true, force: true });
+fs.mkdirSync(PATH);
+
+// TODO: Copy files with esbuild?
+fs.copyFileSync('src/index.html', `${PATH}/index.html`);
+
+const context = await esbuild.context({
+    bundle: true,
+    minify: true,
+    entryPoints: ['src/app.tsx'],
+    outfile: `${PATH}/app.js`,
+    platform: 'browser',
+});
+
+await context.watch()
+
+await context.serve({
+    servedir: PATH,
+})
+.then((serveResult) => { console.log(serveResult); });
