7e90c011997d8b4fce01b905f562ccacb6caf067
0p3r4t0r <robert83sutherland@gmail.com>
2023-12-28 20:29:12 +0900

    feature: log diagram to console.

--- src/components/MainFlow.tsx
+++ src/components/MainFlow.tsx
@@ -10,20 +10,23 @@ import 'reactflow/dist/style.css';
 import { ServiceSelector } from './services/ServiceSelector';
 import { AWS } from './services';
 import { useGamePlanStore } from '../store';
+import { ConsoleLogDiagram } from './utils/ConsoleLogDiagram';
 
-export default function FirstFlow() {
-  const { nodes, edges, onNodesChange, onEdgesChange, onConnect } = useGamePlanStore(
+export default function MainFlow() {
+  const { nodes, edges, onNodesChange, onEdgesChange, onConnect, onInit } = useGamePlanStore(
     (state) => ({
       nodes: state.nodes,
       edges: state.edges,
       onNodesChange: state.onNodesChange,
       onEdgesChange: state.onEdgesChange,
       onConnect: state.onConnect,
+      onInit: state.onInit,
     })
   );
 
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
+      <ConsoleLogDiagram />
       <ReactFlow
         nodes={nodes}
         edges={edges}
@@ -31,6 +34,7 @@ export default function FirstFlow() {
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         nodeTypes={AWS.services}
+        onInit={onInit}
       >
         {/* TODO: use state to avoid prop-drilling? */}
         <ServiceSelector services={AWS.services} />
