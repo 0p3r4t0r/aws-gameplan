511cbe0c5bffb56243f7b1633777910553022bc4
0p3r4t0r <robert83sutherland@gmail.com>
2023-12-28 19:45:09 +0900

    feature: state with zustand

--- src/components/FirstFlow.tsx
+++ src/components/FirstFlow.tsx
@@ -3,30 +3,23 @@ import ReactFlow, {
   MiniMap,
   Controls,
   Background,
-  useNodesState,
-  useEdgesState,
-  addEdge,
   BackgroundVariant,
 } from 'reactflow';
  
 import 'reactflow/dist/style.css';
 import { ServiceSelector } from './services/ServiceSelector';
 import { AWS } from './services';
- 
-// TODO: ensure type property belongs to aws nodes.
-const initialNodes = [
-  { id: '1', type: 'ec2', position: { x: 0, y: 100 }, data: { label: 'EC2' } },
-  { id: '2', type: 'elb', position: { x: 0, y: 200 }, data: { label: 'ELB' } },
-];
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
- 
+import { useGamePlanStore } from '../store';
+
 export default function FirstFlow() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
- 
-  const onConnect = useCallback(
-    (params: Parameters<typeof addEdge>[0]) => setEdges((eds) => addEdge(params, eds)),
-    [setEdges],
+  const { nodes, edges, onNodesChange, onEdgesChange, onConnect } = useGamePlanStore(
+    (state) => ({
+      nodes: state.nodes,
+      edges: state.edges,
+      onNodesChange: state.onNodesChange,
+      onEdgesChange: state.onEdgesChange,
+      onConnect: state.onConnect,
+    })
   );
 
   return (
@@ -40,7 +33,7 @@ export default function FirstFlow() {
         nodeTypes={AWS.services}
       >
         {/* TODO: use state to avoid prop-drilling? */}
-        <ServiceSelector services={AWS.services} nodes={nodes} setNodes={setNodes}/>
+        <ServiceSelector services={AWS.services} />
         <Controls />
         <MiniMap />
         <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
