0343c0b61d1366dea9f52222c555b41ddcd15fc5
0p3r4t0r <robert83sutherland@gmail.com>
2023-12-29 13:41:55 +0900

    feature: add some services, and simple search on the selector

--- src/components/services/ServiceSelector.tsx
+++ src/components/services/ServiceSelector.tsx
@@ -8,17 +8,45 @@ type ServiceSelectorProps = {
 
 export const ServiceSelector = ({ services }: ServiceSelectorProps) => {
     const addNode = useGamePlanStore(state => state.addNode);
+    const [serviceKeys, setServicesKeys] = React.useState(Object.keys(services).sort());
 
     return (
-        <div style={{ position: 'fixed', top: 0, right: 0, zIndex: 100, padding: 10, display: 'flex', flexDirection: 'column', border: 'solid black 1px', backgroundColor: 'white' }}>
-            <p style={{ margin: 0 }}>Services</p>
+        <div style={{
+            position: 'fixed',
+            top: 0,
+            right: 0,
+            zIndex: 100,
+            padding: 10,
+            display: 'flex',
+            flexDirection: 'column',
+            border: 'solid grey 1px',
+            borderRadius: 5,
+            backgroundColor: 'white',
+            height: 400,
+            width: 250,
+        }}>
+            <p style={{ margin: 0 }}>Components</p>
+            <input style={{ margin: '10px 0', width: '100%' }} type="text" placeholder="🔎 Search" onChange={(e) => {
+                const searchString = e.target.value.toLowerCase();
+                if (searchString) {
+                    setServicesKeys(serviceKeys.filter(key => key.toLowerCase().includes(e.target.value.toLowerCase())))
+                } else {
+                    setServicesKeys(Object.keys(services).sort())
+                }
+            }} />
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                overflow: 'scroll',
+            }}>
             {
-                Object.keys(services).sort().map((key) =>
-                    <button key={key} onClick={() => addNode(key)}>
-                        +&nbsp;{key}
+                serviceKeys.map((key) =>
+                    <button style={{ textAlign: 'left', margin: '1px 0' }} key={key} onClick={() => addNode(key)}>
+                        +&nbsp;&nbsp;{key}
                     </button>
                 )
             }
+            </div>
         </div>
     )
 }
