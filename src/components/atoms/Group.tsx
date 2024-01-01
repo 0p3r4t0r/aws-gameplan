f1a5e61baeb6e72b6ea624d78cb26aed3c001a87
0p3r4t0r <robert83sutherland@gmail.com>
2024-01-01 17:55:06 +0900

    fix: use node properties to set initial dimensions for Group

--- src/components/atoms/Group.tsx
+++ src/components/atoms/Group.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Handle, Position, NodeResizer, useNodeId } from 'reactflow';
+import { NodeResizer, useNodeId } from 'reactflow';
 import { useGamePlanStore } from '../../store';
 
 type GroupNodeProps = {
@@ -12,11 +12,12 @@ type GroupNodeProps = {
 export const GroupNode = ({ data, title }: GroupNodeProps) => {
     const deleteNode = useGamePlanStore(state => state.deleteNode);
     const nodeId = useNodeId()!;
+
     return (
-        <div style={{width: 360, height: 360}}>
+        <>
             <img className="group-drag-handle" alt={title} src={data} title={title} width="32" height="32" />
             <button onClick={() => deleteNode(nodeId)} style={{ position: 'absolute', top: 0, right: 0}}>x</button>
             <NodeResizer minHeight={32} minWidth={32} />
-        </div>
+        </>
     )
 }
