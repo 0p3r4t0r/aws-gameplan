4bcadc87addac669677f2df9628408fc6731b71d
0p3r4t0r <robert83sutherland@gmail.com>
2024-03-24 15:06:06 +0900

    add ability to edit node data

--- src/components/atoms/Group.tsx
+++ src/components/atoms/Group.tsx
@@ -1,15 +1,19 @@
-import React, { useCallback } from 'react'
-import { NodeResizer } from 'reactflow'
+import React from 'react'
+import { NodeProps, NodeResizer, useNodeId } from 'reactflow'
+import { useGamePlanStore } from '../../store'
 
 type GroupNodeProps = {
     imgSrc: string
     title: string
-    text?: string
+    data: NodeProps['data']
 }
 
 // https://reactflow.dev/learn/layouting/sub-flows
 // https://reactflow.dev/examples/nodes/resize-rotate
-export const GroupNode = ({ imgSrc, title, text="" }: GroupNodeProps) => {
+// https://stackoverflow.com/questions/73508204/custom-nodes-in-react-flow-saving-additional-data-to-a-node-after-it-has-been-c
+export const GroupNode = ({ imgSrc, title, data }: GroupNodeProps) => {
+    const id = useNodeId()!;
+    const editNodeData = useGamePlanStore((state) => state.editNodeData)
     return (
         <>
             <div style={{ display: 'flex', alignItems: 'center', gap: 5 }}>
@@ -24,8 +28,11 @@ export const GroupNode = ({ imgSrc, title, text="" }: GroupNodeProps) => {
                 <input 
                     className="group-enable-pointer-events" 
                     style={{ flexGrow: 1, marginRight: 5, height: 24, fontSize: 20 }}
-                    type="text"
-                    value={text}
+                    name="text"
+                    value={data.text}
+                    onChange={(e) => {
+                        editNodeData(id, { text: e.target.value })
+                    }}
                 />
             </div>
             <NodeResizer minHeight={32} minWidth={32} />
